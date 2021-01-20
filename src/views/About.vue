0e59eca8a47aca44116e3ee1b0a100d58ef1d468
Daniel Rivera <Danielf.r97@gmail.com>
2021-01-20 14:16:40 -0500

    webpack

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="about">
     <h1></h1>
-    <img src="..\assets\how-hard-disk-drives-work-1.png" />
-    <img src="..\assets\reading-writing-data-1.png" />
-    <img src="..\assets\platters-spindle-1.png" />
-    <img src="..\assets\case-1.png" />
+    <img :src="require('../assets/how-hard-disk-drives-work-1.png')"/>
+    <img :src="require('../assets/reading-writing-data-1.png')"/>
+    <img :src="require('../assets/platters-spindle-1.png')"/>
+    <img :src="require('../assets/case-1.png')"/>
+
     <p>
     <div class="csl-bib-body" style="line-height: 1.35">
       <div class="csl-entry" style="clear: left">
